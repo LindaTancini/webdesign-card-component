eec52b0f89580ccd9949489c8c5bb71e01387f83
LindaTancini <lindatancini95@gmail.com>
2025-08-27 13:12:08 +0200

    stories: create story for discount card , sold out card and change description

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -2,6 +2,8 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ProductCard } from "../components/Card/ProductCard";
 import cuccia from "../assets/cuccia.png";
+import tiragraffi from "../assets/tiragraffi.png";
+import topolini from "../assets/topolini.png";
 
 // Configurazione principale della "storia" del componente Card
 const meta: Meta<typeof ProductCard> = {
@@ -11,7 +13,8 @@ const meta: Meta<typeof ProductCard> = {
   argTypes: {
     // Definizione dei controlli interattivi per modificare le props in Storybook
     image: { control: "text" }, // Campo di testo per la prop "image"
-    badge: { control: "text" }, // Campo di testo per la prop "badge"
+    discount: { control: "text" }, // Campo di testo per la prop "discount"
+    soldOut: { control: "boolean" }, // Checkbox per la prop "soldOut
     title: { control: "text" }, // Campo di testo per la prop "title"
     price: { control: "number" }, // Campo numerico per la prop "price"
     onBuy: { action: "onBuy clicked" }, // Aggiunge un'azione per il click sul bottone
@@ -20,7 +23,8 @@ const meta: Meta<typeof ProductCard> = {
     docs: {
       description: {
         component:
-          "ProductCard è un componente UI che mostra un'immagine, un titolo, un prezzo e un bottone 'Compra ora'. È accessibile via `aria-label` e può essere riutilizzato nel design system.",
+          "Il componente `ProductCard` rappresenta una scheda prodotto completa di immagine, titolo, prezzo e azione di acquisto. " +
+          "Supporta badge per sconti e stato di esaurito, ed è progettato per essere accessibile e facilmente integrabile in un design system.",
       },
     },
     layout: "centered",
@@ -33,9 +37,29 @@ type Story = StoryObj<typeof ProductCard>;
 export const Default: Story = {
   args: {
     image: cuccia, // Immagine di esempio
-    badge: "-10%", // Badge di esempio
     title: "Cuccia Piramide Cleo", // Titolo di esempio
     price: 34.99, // Prezzo di esempio
-    onBuy: () => console.log("Hai cliccato su Compra ora"), // Funzione al click
+    onBuy: () => console.log("Hai cliccato su Aggiungi al carrello"), // Funzione al click
+  },
+};
+
+// Storia della Card con sconto
+export const WithDiscount: Story = {
+  args: {
+    image: tiragraffi,
+    title: "Trixie - Tiragraffi Altea",
+    price: 49.99,
+    discount: "-20%",
+    onBuy: () => console.log("Hai cliccato su Aggiungi al carrello"),
+  },
+};
+
+// Storia della Card con prodotto esaurito
+export const SoldOut: Story = {
+  args: {
+    image: topolini,
+    title: "IMAC - Set 2 Topolini con Coda",
+    price: 4.99,
+    soldOut: true,
   },
 };
